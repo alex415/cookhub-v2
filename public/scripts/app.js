d82adb35ff8913ae00cebf861c1b461437d2c8b8
Alex Chan <alex415@gmail.com>
2015-11-14 19:42:33 -0800

    search form complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,35 @@
 var app = angular.module('recipeApp', ['ngRoute']);
 
+
+// ROUTES
 app.config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
 
-  $routeProvider
-  .when('/', {
-    templateUrl: 'views/index.html',
-    controller: 'MainController'
-  });
+    $routeProvider
+    .when('/', {
+      templateUrl: 'views/index.html',
+      controller: 'MainController'
+      
+});
 
+// SERVICES
 app.service('recipeService', function() {
 
-  $scope.recipes = recipeService.recipes;
-
-  $scope.$watch('recipes', function() {
-    recipeService.recipes = $scope.recipes;
-  });
+  
 
 });
 
+// CONTROLLERS
 app.controller('MainController', ['$scope', '$resource', 'recipeService', function ($scope, $resource, recipeService) {
 
-  $scope.recipes = recipeService.recipes;
+    $scope.recipes = recipeService.recipes;
 
-  $scope.recipeAPI = $resource("/api", { save: {method: 'POST'}, params: {tag: recipe} });
+    $scope.$watch('recipes', function() {
+      recipeService.recipes = $scope.recipes;
+    });
 
-  $scope.recipeResult = $scope.recipeAPI.post({ q: $scope.recipes, });
+    $scope.recipeAPI = $resource("/api", { save: {method: 'POST'}, params: {tag: recipe} });
+
+    $scope.recipeResult = $scope.recipeAPI.post({ q: $scope.recipes, });
 
 }]);
 
